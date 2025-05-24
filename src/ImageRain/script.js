6b0a18c5f98e9ab38a98b3ef6d684cc42d0ec05f
MrMame <hereAtLaptop@github.com>
2025-05-24 23:19:17 +0200

    - Bloecke Bewegen sich im interval - Debug Eingerichtet

--- src/ImageRain/script.js
+++ src/ImageRain/script.js
@@ -1,17 +1,31 @@
 _images = [];
 const START_TOP = 0;
-const IMAGE_WIDTH = 5;
+const IMAGE_WIDTH = 25;
+const SPEED = 1;
+const UNIT = 'px';
 
- 
+setInterval(moveBlocks,1); 
+
+
+function moveBlocks(){
+  _images.forEach(element => {
+    let parsedTop = parseFloat(getComputedStyle(element).top)
+    if(parsedTop<0)parsedTop='100';
+    if(parsedTop>100)parsedTop='0';
+    element.style.top = (parsedTop + SPEED) + UNIT;
+  });
+}
+
+
 
 function addImageBlocks(anzahl = 1){
   for(let ni = 1;ni<anzahl;ni++){
     newImage = document.createElement("img");
     newImage.src = "logo.jpg";
-    newImage.style.width = IMAGE_WIDTH + '%';
+    newImage.style.width = IMAGE_WIDTH + UNIT;
     newImage.style.height = 'auto';
-    newImage.style.left = (getRandomIntInRange(IMAGE_WIDTH,100) - IMAGE_WIDTH) + '%';
-    newImage.style.top = (getRandomIntInRange(START_TOP,100) - IMAGE_WIDTH) + '%';;
+    newImage.style.left = (getRandomIntInRange(IMAGE_WIDTH,100) - IMAGE_WIDTH) + UNIT;
+    newImage.style.top = (getRandomIntInRange(START_TOP,100) - IMAGE_WIDTH) + UNIT;;
     document.getElementById('TestArea').appendChild(newImage);
     _images.push(newImage);
   }
