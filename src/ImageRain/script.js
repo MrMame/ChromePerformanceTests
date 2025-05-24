28dbb36fb3e5aca23241d67713b64e08a72477c6
MrMame <hereAtLaptop@github.com>
2025-05-24 23:49:55 +0200

    - Images spawnen über gesamte testarea

--- src/ImageRain/script.js
+++ src/ImageRain/script.js
@@ -8,24 +8,28 @@ setInterval(moveBlocks,1);
 
 
 function moveBlocks(){
+  let areaHeight = document.getElementById('TestArea').clientHeight;
   _images.forEach(element => {
-    let parsedTop = parseFloat(getComputedStyle(element).top)
-    if(parsedTop<0)parsedTop='100';
-    if(parsedTop>100)parsedTop='0';
-    element.style.top = (parsedTop + SPEED) + UNIT;
+    let elementTop = parseFloat(getComputedStyle(element).top)
+    if(elementTop<0)element.speed=SPEED;
+    if((elementTop+IMAGE_WIDTH)>areaHeight)element.speed=SPEED*-1;
+    element.style.top = (elementTop + element.speed) + UNIT;
   });
 }
 
 
 
 function addImageBlocks(anzahl = 1){
+  let areaHeight = document.getElementById('TestArea').clientHeight;
+  let areaWidth = document.getElementById('TestArea').clientLeft
   for(let ni = 1;ni<anzahl;ni++){
     newImage = document.createElement("img");
     newImage.src = "logo.jpg";
     newImage.style.width = IMAGE_WIDTH + UNIT;
     newImage.style.height = 'auto';
-    newImage.style.left = (getRandomIntInRange(IMAGE_WIDTH,100) - IMAGE_WIDTH) + UNIT;
-    newImage.style.top = (getRandomIntInRange(START_TOP,100) - IMAGE_WIDTH) + UNIT;;
+    newImage.style.left = (getRandomIntInRange(IMAGE_WIDTH,areaWidth) - IMAGE_WIDTH) + UNIT;
+    newImage.style.top = (getRandomIntInRange(START_TOP,areaHeight) - IMAGE_WIDTH) + UNIT;
+    newImage.speed = SPEED;
     document.getElementById('TestArea').appendChild(newImage);
     _images.push(newImage);
   }
